3dcf445926809ea6b1ac787f9b60f6903ef0e825
bslqy <853185345@qq.com>
2016-09-28 16:04:25 +0800

    Update P1.c
    Finish copying the keys

--- P1.c
+++ P1.c
@@ -1,12 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX 4400*8  //one row of double = sizeof(Double) *4400
+#define MAX 1000000  //one row of double = sizeof(Double) *4400
+#define LINE_LENGTH 15
+
+void readKey(char* fileName)
+{
+
+char* keys[4400][LINE_LENGTH];
+
+int i = 0;
+  FILE *fp;
+  char *string=(char*)calloc(LINE_LENGTH,sizeof(char));
+
+  fp = fopen(fileName, "r");
+
+  while(1)
+    {
+      if(fgets(string, LINE_LENGTH, fp) == NULL)
+         break;
+      fgetc(fp);
+
+      printf("\nline[%d] = %s\n", i+1, string);
+      strcpy(keys[i],string);
+
+      i++;
+    }
+
+
+printf("%s hga",&keys[0]);
+  fclose(fp);
+
+}
+
+
 
 void readMatrix(char* fileName)
 {
 	FILE *fp;
 	int i, j;
 	char s[MAX], ch;
+
+
 
 
 	if ((fp = fopen(fileName, "r")) == NULL) {
@@ -17,7 +51,7 @@ void readMatrix(char* fileName)
   while (fgets(s, MAX, fp) != NULL) //Count the line
     row++;
 
-  rewind(fp); //
+  rewind(fp); //Going back to the head of the matrix
 
   int col = 0; //Count the col
   while (ch != '\n') //(ch=fgetc(fp))!='\n'&&(ch=fgetc(fp))!='\r'
@@ -25,7 +59,7 @@ void readMatrix(char* fileName)
     if (ch == ' ') col++;
     ch = fgetc(fp);
   }
-  col++; // 
+  col++; //
 
   double** jz = malloc(row * sizeof(double*)); //Allocate spaces
   for (i = 0; i < row; i++)
@@ -48,10 +82,13 @@ void readMatrix(char* fileName)
   fclose(fp);
 
   printf("%d row, %d  col ",row,col);
-}
+    }
+
+
 
 int main(void) {
 
 
+readKey("keys.txt");
 
 }
