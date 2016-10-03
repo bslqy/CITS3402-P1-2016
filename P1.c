2be71aaf6749de9ae9ca307089236b089596d246
bslqy <853185345@qq.com>
2016-10-03 12:55:25 +0800

    Update P1.c

--- P1.c
+++ P1.c
@@ -5,10 +5,29 @@
 #define ROW_SIZE 4400
 #define COL_SIZE 500
 
+
+/** individual block **/
+struct block
+{
+    int row1;
+    int row2;
+    int row3;
+    int row4;
+    char* signature;
+};
+
+/** A collection of blocks for one column **/
+struct Blocks
+{
+    struct block;
+    int COL_INDEX;
+};
+
+
 char* readKey(char* fileName)
 {
 
-char* keys[4400][LINE_LENGTH];
+char keys[4400][LINE_LENGTH];
 
   int i = 0;
   FILE *fp;
@@ -30,11 +49,18 @@ char* keys[4400][LINE_LENGTH];
     }
 
 
-printf("%s\n",&keys[4399]);
   fclose(fp);
   return keys;
 
 }
+
+char* getKey()
+{
+
+}
+
+
+
 
 /** use to return one col of the matrix after the col is scanned for the first time */
 double* readCol(double** jz, int col_number,int row_size)
@@ -61,7 +87,8 @@ return col;
 }
 
 
-/** use to return one col of the matrix after the col is scanned for the first time */
+
+/** use to return a collection of DIA neighbours given a fixed row (start)  */
 int* collection(double* one_column, int start,int row_size)
 {
     // Put itself in a collection e.g. {1}
@@ -85,7 +112,6 @@ int* collection(double* one_column, int start,int row_size)
 
         collection_for_one_row= (int*)realloc(collection_for_one_row,size_of_set*sizeof(int)); //Resize the array, increase by one.
         collection_for_one_row[size_of_set-1] = i+1; // Add the index of the neighbour into the collection array.
-
     }
 
 
@@ -104,6 +130,107 @@ int* collection(double* one_column, int start,int row_size)
 
  }
 
+ int** combination(int* array,int n, int m)
+ {
+
+
+ }
+
+ /** Function to add two signature together */
+
+ char* Add_Two_Num(char* num1, char* num2){
+
+
+    int len1,len2;
+    int carry = 0;
+    int i,j,k;
+    int temp;
+
+    len1 = strlen(num1);
+    len2 = strlen(num2);
+    char* result;
+    k = 0;
+
+    // (Maximum_size) N*4  is one more than the size_of_N
+    if((result=malloc(len1*sizeof(char)))==NULL)
+    {
+        exit(1);
+    }
+
+
+    //Add the number from the lowest place   i.e. from right to left.
+    for(i=len1-1,j=len2-1;i>=0 && j>=0;i--,j--){
+        temp = num1[i]-'0'+num2[j]-'0'+carry; //Convert a char to int.
+        if(temp>=10){
+            temp -= 10;
+            carry = 1;
+        }else{
+            carry = 0;
+        }
+
+        result[k] = temp+'0';       //store the results as strings
+        k++;
+    }
+
+    //Finishing the rest of the addition, assuming two integer are of difference size.
+    while(i>=0){
+        temp = num1[i]-'0'+carry;
+        if(temp>=10){
+            temp -= 10;
+            carry = 1;
+        }else{
+            carry = 0;
+        }
+
+        result[k] = temp+'0';
+        i--;
+        k++;
+    }
+
+    while(j>=0){
+        temp = num2[j]-'0'+carry;
+        if(temp>=10){
+            temp -= 10;
+            carry = 1;
+        }else{
+            carry = 0;
+        }
+        result[k] = temp+'0';
+        j--;
+        k++;
+    }
+
+    if(carry){      //if there is still a carry for the addition of two number at the head
+        result[k] = 1+'0';
+        result = (char*)realloc(result,(len1+1)*sizeof(char));
+        }
+    else{           //move the index back to the last place of the result array
+        k--;
+    }
+
+    while(k>=0){   //print the result reversely
+        printf("%c ",result[k]);
+        printf("size = %d\n",k);
+        k--;
+    }
+    printf("\n");
+    printf("length of result = %d\n",strlen(result));
+    return result;
+}
+
+struct block Create_Block(int* arr)
+{
+    struct block b;
+    b.row1 = arr[0];
+    b.row2 = arr[1];
+    b.row3 = arr[2];
+    b.row4 = arr[3];
+//    b.signature = malloc(strlen(Add_Two_Num(Add_Two_Num(keys[b.row1],keys[b.row2]),Add_Two_Num(keys[b.row1],keys[b.row2]))));
+  //  strcpy(b.signature,Add_Two_Num(Add_Two_Num(keys[b.row1],keys[b.row2]),Add_Two_Num(keys[b.row1],keys[b.row2])));
+
+    return b;
+};
+
 
 
 
@@ -113,7 +240,7 @@ void readMatrix(char* fileName)
  int i,j;
  char s[MAX],ch;
 
- if((fp=fopen("3.txt","r"))==NULL)
+ if((fp=fopen("fileName","r"))==NULL)
  {
   printf("can not open this file!\n");
   exit(1);
@@ -161,79 +288,24 @@ jz[i]=malloc(col*sizeof(double));
        int* one_collection =collection(c,0,3);
 
 
-
-// for(i=0;i<col;i++){//Print the matrix
-//    for(j=0;j<row-1;j++){
-//       // printf("%lf first row, %lf second row\n absolute value of them is %lf\n ",jz[j][i],jz[j+1][i],fabs(jz[j][i]-jz[j+1][i]));
-//
-//        int number_of_neighbour = 1; // Count itself as a neighbour
-//        int* collection_for_one_row = malloc(sizeof(int));
-//
-//
-//       // j is the benchmark of the dia checking operation
-//      //i.e. fixing j and find the DIA down the row
-//        collection_for_one_row[0] = j;
-//
-//    if(fabs(jz[j][i]-jz[j+1][i]) < 0.4)  //If two row are in the same DIA
-//    {
-//        printf("%lf and %lf are in the dia\n" ,jz[j+1][i],jz[j][i]);
-//        //Put the row index in an array
-//        number_of_neighbour++;
-//        realloc(collection_for_one_row,number_of_neighbour*sizeof(int)); //Resize the array, increase by one.
-//        collection_for_one_row[number_of_neighbour] = j+1; // Add the index of the neighbour into the collection array.
-//    }
-//  }
-
-
  fclose(fp);
 
   printf("%d row, %d  col ",row,col);
 }
-
-
-
-
-
-
-void swap(int a, int b)
-{
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-
-}
-
-void AllRange(int array[],int start , int len)
-{
-    if(start == len-1) //Base case
-    {
-        for(int j = 0; j<len;j++)
-        {
-            printf("%d ",array[j]);
-        }
-        printf("\n");
-        return;
-    }
-
-        for(int i=start;i<len-1;i++)
-        {
-            swap(array[start],array[i]);
-            AllRange(array,start + 1,len);
-            swap(array[start],array[i]);
-        }
-
-
-}
-
-
-
 
 int main(void) {
 
 //readKey("keys.txt");
 //readMatrix("3.txt");
 
-readMatrix("2.txt");
+/** store the keys **/
+char* keys = readKey("keys.txt");
+//int* arr = {1,2,3,4};
+//struct block b = Create_Block(arr);
+//printf("%c",b.signature[0]);
+for(int i = 0;i<15;i++){
+    printf("%c,",keys[4355*15+i]);
+}
 
+//readMatrix("2.txt");
 }
