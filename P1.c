04bcf6079ca83242929ef9415f4dc1e3c6a4c827
bslqy <853185345@qq.com>
2016-10-22 14:59:14 +0800

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,5 +1,20 @@
+/*
+ * Student Number1: 21448107
+ * Student Name1: Cikang Li
+ *
+ * Student Number2: 21480279
+ * Student Name2: Jia Liao
+ */
+
+
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+//#include <omp.h>
+
 #define MAX 1000000  //one row of double = sizeof(Double) *4400
 #define LINE_LENGTH 15
 #define ROW_SIZE 4400
@@ -7,506 +22,362 @@
 #define DIA 0.000001
 #define M  4        // SIZE OF COMBINATION
 
-
-
 /** individual block **/
-struct block
-{
+struct block{
     int col_index;
     int row1;
     int row2;
     int row3;
     int row4;
-    char signature[15];
+    long long signature;
 };
 
+long long* KEYS;   //GLOBAL KEY STORAGE
 
-char* KEYS;                 //  GLOBAL KEY ARRAY
 
-    //  GLOBAL VARIAABLES USED FOR BLOCK CREATION
+//  GLOBAL VARIAABLES USED FOR BLOCK CREATION
 struct block b;             //  RECYCLCABLE SINGLE BLOCK
 struct block* Big_Block;    //  COLLECTION OF ALL BLOCK
 int NUM_OF_BLOCK = 0;       //  TOTAL NUMBER OF BLOCK
 
     //  GLOBAL VARIABLE USED FOR COMBINATION
 int SIZE_OF_COLLECTION=0;     // SIZE OF SET FOR COMBINATION CALCULATION N choose M
+//int SIZE_OF_COMBINATION = 1;    // SIZE OF COMBINATION nCm
+
+/*
 int queue[4400] = {0} ;           // THE TEMPERORY ARRAY THAT STORE THE INSTANCE OF THE CONBINATION
 int result[M] = {0};        // THE ARRAY THAT SAVE THE RESULT OF ONE COMBINATION
 int top = 0;                // THE POINTER OF RECURSION
+*/
 
+void readKey(char* fileName){
 
+    KEYS = malloc(4400*sizeof(long long));
 
+	int i = 0;
+	FILE *fp;
 
-char* readKey(char* fileName)
-{
+	fp = fopen(fileName, "r");
 
-char keys[4550][LINE_LENGTH]; // 4400 ROWS SIZE HAS OVERFLOW PROBLEM
+	while(i<4400){
+		if(!fscanf(fp,"%lli",&KEYS[i])){
+			break;
+		}
 
-  int i = 0;
-  FILE *fp;
-  char *string=(char*)calloc(LINE_LENGTH,sizeof(char));
+		fgetc(fp);         //GET RID OF THE WHITE SPACE
 
-  fp = fopen(fileName, "r");
+		i++;
+	}
 
-  while(1)
-    {
-      if(fgets(string, LINE_LENGTH, fp) == NULL)
-         break;
-         fgetc(fp);         //GET RID OF THE WHITE SPACE
-
-
-     // printf("\nline[%d] = %s\n", i+1, string);
-      strcpy(keys[i],string);
-
-      i++;
-    }
-
-
-  fclose(fp);
-  return keys;
+	fclose(fp);
 
 }
 
     /** FUNCTION THAT OBTAIN THE CORRESPOINDING KEY GIVEN A ROW NUMBER **/
 
-char* getKey(int row_index)
-{
-    char* temp = malloc(LINE_LENGTH*sizeof(char));
-    for(int i = 0;i<LINE_LENGTH;i++){
-        temp[i] = KEYS[LINE_LENGTH*row_index+i];
-    }
-
-    return temp;
-
+long long getKey(int row_index){
+	return KEYS[row_index];
 }
 
 
 
 
 /** use to return one col of the matrix after the col is scanned for the first time */
-double* readCol(double** jz, int col_number,int row_size)
-{
+double* readCol(double** jz, int col_number, int row_size){
 
-	//printf("%d column is:\n",col_number);
+	double* col;
 
-double* col;
-if((col = malloc(row_size* sizeof(double)))!=NULL)
-{
-    for(int i=0;i<row_size;i++)
-{
-    col[i] = jz[i][col_number];
+	if((col = malloc(row_size* sizeof(double))) != NULL){
+		for(int i = 0; i < row_size; i++){
+			col[i] = jz[i][col_number];
+		}
+	}
+
+	return col;
 }
-}
-
-return col;
-
-}
-
 
 
 /** use to return a collection of DIA neighbours given a fixed row (start)  */
-int* collection(double* one_column, int start,int row_size)
-{
+int* collection(double* one_column, int start,int row_size){
     // Put itself in a collection e.g. {1}
      int size_of_set = 1;
-     int* collection_for_one_row = malloc(size_of_set*sizeof(int));
-      collection_for_one_row[0] = start;
-
-
-    for(int i=start;i<row_size-1;i++){
-       // START is the benchmark of the dia checking operation
-      //i.e. fixing START and find the DIA down the row
-      // Put itself in a collection e.g. {START, START+1,START+2 ...}
-
-    if(fabs(one_column[start]-one_column[i+1]) <= DIA)  //If two row are in the same DIA
-    {
-
-        printf("%lf and %lf are in the dia\n" ,one_column[start],one_column[i+1]);
-
-        //Put the row index in an array
-        size_of_set++;
-        collection_for_one_row= (int*)realloc(collection_for_one_row,size_of_set*sizeof(int)); //Resize the array, increase by one.
-        collection_for_one_row[size_of_set-1] = i+1; // Add the index of the neighbour into the collection array.
-    }
-  }
-  SIZE_OF_COLLECTION = size_of_set;  // MODIFY THE ARRAY SIZE N FOR COMBINATION.
-
-
- // printf("size of set is %d\n",size_of_set);
-
-
-//    if(size_of_set<4){
-//        return ;
-//    }
-//    else{
-//     printf("size of set is %d\n",size_of_set);
-//      for(int i=0;i<size_of_set;i++)
-//    {
-//      printf("%d ",collection_for_one_row[i]);
-//      printf("\n");
-//    }
-//  }
-
-    //  ONLY INTERESTED IN THE SET WITH SIZE>4
-
-     printf("size of set is %d\n",size_of_set);
-      for(int i=0;i<size_of_set;i++)
-    {
-      printf("%d ",collection_for_one_row[i]);
-      printf("\n");
-    }
-    return  collection_for_one_row;
-
-
- }
-
-
-
- void comb(int* input,int s, int n, int m)
- {
-     int i;
-
-     if (s > n)
-         return ;
-
-     if (top == m)
-     {
-
-         for (i = 0; i <m; i++)
-         {
-             result[i] = queue[i];   // SAVE THE RESULT TO THE GLOBAL ARRAY FOR ONE PARTICULAR INSTANCE OF THE COMBINATION
-             //printf("%c",result[i]);
-             printf("%d\n", queue[i]);
-         }
-
-         for(int i=0;i<m;i++)
-        printf("This is result:  %d\n",result[i]);
-
-     }
-
-     queue[top++] = input[s];
-     comb(input,s+1, n, M);
-     top--;
-     comb(input,s+1, n, M);
-
- }
-
-
-/** REVSER THE RESULT STRING FOR Add_Two_Num() FUNCTION **/
-
-char *revstr(char *str, size_t len)
-{
-
-    char    *start = str;
-    char    *end = str + len - 1;
-    char    ch;
-
-    if (str != NULL)
-    {
-        while (start < end)
-        {
-            ch = *start;
-            *start++ = *end;
-            *end-- = ch;
-        }
-    }
-    return str;
-}
-
-
-
- /** FUNCTION TO ADD TWO SIGNATURE TOGETHER */
-
- char* Add_Two_Num(char* num1, char* num2){
-
-    char* result;
-    int len1,len2;
-    int carry = 0;
-    int i,j,k;
-    int temp;
-
-    len1 = strlen(num1);
-    len2 = strlen(num2);
-    k = 0;
-
-    // (Maximum_size) N*4  is one more than the size_of_N
-    if((result=malloc((len1+1)*sizeof(char)))==NULL)
-    {
-        exit(1);
-    }
-
-
-    //Add the number from the lowest place   i.e. from right to left.
-    for(i=len1-1,j=len2-1;i>=0 && j>=0;i--,j--){
-        temp = num1[i]-'0'+num2[j]-'0'+carry; //Convert a char to int.
-        if(temp>=10){
-            temp -= 10;
-            carry = 1;
-        }else{
-            carry = 0;
-        }
-
-        result[k] = temp+'0';       //store the results as strings
-        k++;
-    }
-
-    //Finishing the rest of the addition, assuming two integer are of difference size.
-    while(i>=0){
-        temp = num1[i]-'0'+carry;
-        if(temp>=10){
-            temp -= 10;
-            carry = 1;
-        }else{
-            carry = 0;
-        }
-
-        result[k] = temp+'0';
-        i--;
-        k++;
-    }
-
-    while(j>=0){
-        temp = num2[j]-'0'+carry;
-        if(temp>=10){
-            temp -= 10;
-            carry = 1;
-        }else{
-            carry = 0;
-        }
-        result[k] = temp+'0';
-        j--;
-        k++;
-    }
-
-    if(carry){      //if there is still a carry for the addition of two number at the head
-        result[k] = 1+'0';
-        result = (char*)realloc(result,(len1+2)*sizeof(char));
-        }
-    else{           //move the index back to the last place of the result array
-        k--;
-    }
-
-    while(k>=0){   //print the result reversely
-        printf("%c ",result[k]);
-        printf("size = %d\n",k);
-        k--;
-    }
-    printf("\n");
-   printf("length of result = %d\n",strlen(result));
-
-    //REVESRE THE RESULT ARRAY;
-    revstr(result,strlen(result));
-    return result;
-}
-
-
-double** readMatrix(char* fileName){
-
-	FILE *fp;
-	int i,j;
-	char s[MAX],ch;
-
-	if((fp = fopen(fileName,"r")) == NULL){
-		printf("can not open this file!\n");
-		exit(1);
- 	}
-
-	int row=0;
-
-	 while(fgets(s,MAX,fp)!=NULL)//Count the line
-		 row++;
-
-	 rewind(fp);//
-
-	 int col=0;//Count the col
-
-	 while(ch != '\n')//(ch=fgetc(fp))!='\n'&&(ch=fgetc(fp))!='\r'
-	 {
-		 if(ch == ',')
-			 col++;
-		 ch=fgetc(fp);
-
-		 //printf("this is the m %d \n", col);
-
-
-	 }
-
-	 col++;//
-
-	 //printf("instance: %d  col \n",col);
-
- 	double** jz= malloc(row*sizeof(double*));//Allocate spaces
-
-	for(i=0;i<row;i++){
-		jz[i]=malloc(col*sizeof(double));
-	}
-
-	 rewind(fp);
-
-	 for(i=0;i<row;i++){//Read in the data
-		 for(j=0;j<col;j++){
-
-			 if (!fscanf(fp, "%lf%*[,]", &jz[i][j]))
-			 {
-
-					 break;
-			 }
-
-			 //printf("isthisit? %lf, row: %d, col: %d \n, rowlim: %d, collim: %d", jz[i][j], i, j, row, col);
+     int* collection_for_one_row = malloc(size_of_set * sizeof(int));
+
+	 collection_for_one_row[0] = start;
+
+	 for(int i = start; i < row_size-1; i++){
+     // START is the benchmark of the dia checking operation
+     //i.e. fixing START and find the DIA down the row
+     // Put itself in a collection e.g. {START, START+1,START+2 ...}
+
+		 if(fabs(one_column[start]-one_column[i+1]) <= DIA)  {    //If two row are in the same DIA
+			 //Put the row index in an array
+			 size_of_set++;
+			 collection_for_one_row = (int*)realloc(collection_for_one_row,size_of_set * sizeof(int)); //Resize the array, increase by one.
+			 collection_for_one_row[size_of_set - 1] = i + 1; // Add the index of the neighbour into the collection array.
 		 }
 	 }
 
- fclose(fp);
+	 SIZE_OF_COLLECTION = size_of_set;  // MODIFY THE ARRAY SIZE N FOR COMBINATION.
+	// printf("Inside the collection function, what is the SIZE_OF_COLLECTION? %d\n",SIZE_OF_COLLECTION);
 
- printf("%d row, %d  col \n",row,col);
 
- return jz;
-}
+/*
+	 if(size_of_set > 4){
+		 for(int i=0;i<size_of_set;i++){
+			 printf("%d ",collection_for_one_row[i]);
+		 }
+		 printf("\n");
+
+	 }
+
+	 */
+	 return collection_for_one_row;
+ }
+
+
+
+ /** modify the size of nCm for different input set **/
+ /** nCm = n!/m!(n-m)! **/
+ /** Use natural log to reduce the size **/
+
+ int get_combination_size(int n, int m ){
+ 	return  floor(round(exp(lgamma(n+1)-lgamma(n-m+1))/tgamma(m+1)));
+ }
+
+
+ int** combNonRec(int collect[], int sizeOfColl, int sizeOut){
+
+
+     int size_of_combination = get_combination_size(sizeOfColl,sizeOut);
+	 int index,i,*p;
+     int** finalR = (int**)malloc(sizeof(int*) * size_of_combination);
+
+     for(int i = 0; i < size_of_combination; i++){
+		 finalR[i] = (int*)malloc(sizeof(int)*4);
+	 }
+
+     p = (int*)malloc(sizeof(int) * sizeOut);
+
+	 int thisInd = 0;
+	 index = 0;
+
+     p[index] = 0;//取第一个元素
+
+     while(true){
+		 if(p[index]>=sizeOfColl){//取到底了，回退
+             if(index==0){//各种情况取完了，不能再回退了
+                 break;
+             }
+
+             index--;//回退到前一个
+             p[index]++;//替换元素
+         }else if(index == sizeOut - 1){//取够了，输出
+             for(i = 0; i < sizeOut; i++){
+				  finalR[thisInd][i]  = collect[p[i]];
+             }
+
+			 thisInd++;
+             p[index]++; //替换元素
+         }else{//多取一个元素
+			 index++;
+             p[index] = p[index - 1] + 1;
+         }
+
+     }
+
+	 free(p);
+
+/*
+    for(int i=0;i<size_of_combination;i++)
+    {
+         printf("List %d:\n",i);
+        for(int j=0;j<4;j++)
+        {
+            printf("%d ",finalR[i][j]);
+        }
+         printf("\n");
+         printf("\n");
+    }
+
+    */
+     return finalR;
+ }
 
 
 
 /** RECYCLING THE GLOBAL STRUCT B **/
-void Create_Block(int arr[],int COL_INDEX)
-{
+void create_Block(int arr[], int COL_INDEX){
+
     b.col_index = COL_INDEX;
     b.row1 = arr[0];
     b.row2 = arr[1];
     b.row3 = arr[2];
     b.row4 = arr[3];
-    strcpy(b.signature, Add_Two_Num(Add_Two_Num(getKey(b.row1),getKey(b.row2)),Add_Two_Num(getKey(b.row3),getKey(b.row4))));
-
+    b.signature = getKey(arr[0])+getKey(arr[1])+getKey(arr[2])+getKey(arr[3]);
 }
 
 
-/** RECYCLING THE GLOBAL STRUCT B **/
 
-void Add_To_Block_Collection()
-{
+
+/** RECYCLING THE GLOBAL STRUCT B **/
+void add_To_Block_Collection(){
     //FIRST ALLOCATION
     if(NUM_OF_BLOCK == 0){
-        if((Big_Block =(malloc(sizeof(struct block))))== NULL)
-        {
+        if((Big_Block = (malloc(sizeof(struct block)))) == NULL){
             printf("Memory Allocation fails\n");
             exit(1);
         }
+
         Big_Block[NUM_OF_BLOCK] = b;// STORE THE INSTANCE OF BLOCK INTO THE COLLECTION , b IS THE RECYCLABLE BLOCK WHICH GETS FREED EVERYTIME
         NUM_OF_BLOCK++;
-
     }
 
     else{
         //SEQUENTIAL ACCESS
         NUM_OF_BLOCK++; //ADD ONE MORE BLOCK TO THE COLLECTION OF BLOCKS
         Big_Block = realloc(Big_Block,NUM_OF_BLOCK * sizeof(struct block));
-        Big_Block[NUM_OF_BLOCK-1] = b; // STORE THE INSTANCE OF BLOCK INTO THE COLLECTION, b IS THE RECYCLABLE BLOCK WHICH GETS FREED EVERYTIME
+        Big_Block[NUM_OF_BLOCK - 1] = b; // STORE THE INSTANCE OF BLOCK INTO THE COLLECTION, b IS THE RECYCLABLE BLOCK WHICH GETS FREED EVERYTIME
     }
-
-        printf("This is big block 1 signature %s\n",Big_Block[NUM_OF_BLOCK-1].signature);
-
 }
 
-///** FINAL OUTPUT **/
-//void Collision(int start)
-//{
-//    //  FIXING THE ROW AND COMPARE THE SIGNATURE DOWNWARD, SAME AS FINDING DIA NEIGHBOUR
-//    for(int i = start;i<NUM_OF_BLOCK-1;i++)
-//    {
-//
-//            if((strcmp(Big_Block[i].signature,Big_Block[i+1].signature)==0) && (Big_Block[i].col_index != Big_Block[i+1].col_index))
-//            {
-//
-//                printf("Block %d and Block %d collide\n",i,i+1);
-//                printf("Row Index From Block %d: %d %d %d %d \n",i,Big_Block[i].row1,Big_Block[i].row2,Big_Block[i].row3,Big_Block[i].row4);
-//                printf("Row Index From Block %d: %d %d %d %d \n",i+1,Big_Block[i+1].row1,Big_Block[i+1].row2,Big_Block[i+1].row3,Big_Block[i+1].row4);
-//
-//            }
-//
-//    }
-// printf("HHAHA");
-//
-//
-//}
 
 /** FINAL OUTPUT **/
-void Collision()
-{
+void collision(){
     //  FIXING THE ROW AND COMPARE THE SIGNATURE DOWNWARD, SAME AS FINDING DIA NEIGHBOUR
-    for(int i = 0;i<NUM_OF_BLOCK-1;i++)
-    {
-        for(int j = i+1;j<=NUM_OF_BLOCK-1;j++){
-               // printf("j  is %d\n",j);
-               // printf("i  is %d\n",i);
-            if((strcmp(Big_Block[i].signature,Big_Block[j].signature)==0) && (Big_Block[i].col_index != Big_Block[j].col_index))
-            {
-                printf("Block %d and Block %d collide\n",i,j);
+    for(int i = 0; i < NUM_OF_BLOCK - 1; i++){
+        for(int j = i + 1; j <= NUM_OF_BLOCK - 1; j++){
+            if((Big_Block[i].signature == Big_Block[j].signature)  && (Big_Block[i].col_index != Big_Block[j].col_index)){
+                printf("Block %d from Col %d and Block %d from Col %d collide\n",i,Big_Block[i].col_index,j,Big_Block[j].col_index);
                 printf("Row Index From Block %d: %d %d %d %d \n",i,Big_Block[i].row1,Big_Block[i].row2,Big_Block[i].row3,Big_Block[i].row4);
                 printf("Row Index From Block %d: %d %d %d %d \n",j,Big_Block[j].row1,Big_Block[j].row2,Big_Block[j].row3,Big_Block[j].row4);
 
-            }
-            else{
-                printf("No Collision Happens for Block %d and Block %d \n",i,j);
+            }else{
+               // printf("No collision Happens for Block %d and Block %d \n",i,j);
             }
         }
-
     }
+}
 
 
+double** readMatrix(char* fileName){
 
+	FILE *fp;
+	int i, j;
+	char s[MAX], ch;
+
+	if((fp = fopen(fileName, "r")) == NULL){
+		printf("can not open this file!\n");
+		exit(1);
+ 	}
+
+	int row = 0;
+
+	 while(fgets(s, MAX, fp)!=NULL){//Count the line
+		 row++;
+	 }
+
+	 rewind(fp);//
+
+	 int col = 0;//Count the col
+
+	 while(ch != '\n'){
+		 if(ch == ','){
+			 col++;
+		 }
+
+		 ch = fgetc(fp);
+	 }
+
+	 col++;
+
+	 double** jz = malloc(row * sizeof(double*));//Allocate spaces
+
+	 for(i = 0; i < row; i++){
+		 jz[i] = malloc(col * sizeof(double));
+	 }
+
+	 rewind(fp);
+
+	 for(i = 0; i < row; i++){//Read in the data
+		 for(j = 0; j < col; j++){
+			 if(!fscanf(fp, "%lf%*[,]", &jz[i][j])){
+					 break;
+			 }
+		 }
+	 }
+
+	 fclose(fp);
+
+	 printf("%d row, %d  col \n",row,col);
+
+	 return jz;
 }
 
 
 
 /******** ======== *********/
 
+
 int main(void) {
 
-	//readMatrix("data.txt");
 
-/** store the keys **/
 
-   // KEYS = readKey("keys.txt");
+	clock_t begin = clock();
+
+	readKey("keys.txt");
 	double** jz = readMatrix("data.txt");
 
 
-//printf("Finish!!!!!!!!\n",getKey(1));
-//	int arr1[4] = {4399,4398,4397,4396};
-//	int arr2[4] = {1,2,3,4};
-//    Create_Block(arr1,1);
-//    Add_To_Block_Collection();
-//
-//    printf("COL_INDEX : %s\n",Big_Block[0].signature);
-//
-//    Create_Block(arr1,2);
-//    Add_To_Block_Collection();
-//    printf("COL_INDEX : %s\n",Big_Block[1].signature);
-//
-//    Create_Block(arr1,2);
-//    Add_To_Block_Collection();
-//    printf("COL_INDEX : %s\n",Big_Block[2].signature);
-//
-//
-//    Collision();
-//    free(Big_Block); //IN THE END
+	//#pragma omp parallel for private(i) shared(ROW_SIZE,NUM_OF_BLOCK,Big_Block,b,SIZE_OF_COLLECTION,SIZE_OF_COMBINATION){
+	for(int i = 0; i < 498; i++){  //423 & 499 broken
 
-    printf("Size Before one_collection is %d\n",SIZE_OF_COLLECTION);
+		printf("THIS IS COLUMN %d\n", i);
 
-    double*c  = readCol(jz,499,4400);
-    int* one_collection = collection(c,0,4400);
-    printf("Size from one_collection is %d\n",SIZE_OF_COLLECTION);
+		double*c  = readCol(jz,i,4400);
+
+		// #pragma omp prallel for private(c,j) shared(ROW_SIZE,NUM_OF_BLOCK,Big_Block,b,SIZE_OF_COLLECTION,SIZE_OF_COMBINATION){
+		for(int j = 0; j < 4400; j++){
+
+           // printf("This is fixed row %d !!!!!!!!!!\n",j);
+			int* one_collection = collection(c,j,4400);
+
+			// MODIFY THE DYMANIC ALLOCATION OF SPACES (SIZE_OF_COMBINATION) IN combNonRec() function.
+
+				if(get_combination_size(SIZE_OF_COLLECTION,M)>= 4){
+				//GET THE 2D-ARRAY OF COMBINATION
+				 int** rrr = combNonRec(one_collection, SIZE_OF_COLLECTION ,M);
+
+				 for(int ii = 0; ii < get_combination_size(SIZE_OF_COLLECTION,M); ii++){
+					 create_Block(rrr[ii],i);  // first arg: int[] ; second arg: col_number;
+					 add_To_Block_Collection();
+					// printf("\n");
+				 }
+
+				 free(rrr);
+			 }
 
 
-    printf("Change the size of collection to 5 \n",SIZE_OF_COLLECTION);
+				// free(one_collection);
+			 }
 
-    SIZE_OF_COLLECTION = 5;
+       //OpenMP for j
+			free(c);
+		}
+   // OpenMP for i
+		collision();
+		free(Big_Block);
 
-    printf("After changing the size is %d  \n",SIZE_OF_COLLECTION);
+		clock_t end = clock();
+		double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		printf("Total time spent : %lf\n",time_spent);
 
-     int array[] = {1,2,3,4,5};
-     printf("\ncombination():\n");
-     comb(one_collection,0,SIZE_OF_COLLECTION,M);
 
-     printf("Size After Combination is %d\n",SIZE_OF_COLLECTION);
-     printf("Finish!");
+
 
 
 }
+
+
+
+
